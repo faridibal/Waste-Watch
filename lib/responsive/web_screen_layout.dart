005cda864b84bdf43b54bf24ca574603db779034
Farid Ibañez <faridibal@gmail.com>
2023-05-14 22:15:17 +0100

    arreglando fallos

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -74,13 +74,6 @@ class _WebScreenLayoutState extends State<WebScreenLayout> {
           ),
           IconButton(
             icon: Icon(
-              Icons.favorite,
-              color: _page == 3 ? primaryColor : secondaryColor,
-            ),
-            onPressed: () => navigationTapped(3),
-          ),
-          IconButton(
-            icon: Icon(
               Icons.person,
               color: _page == 4 ? primaryColor : secondaryColor,
             ),
--- lib/widgets/comment_card.dart
+++ lib/widgets/comment_card.dart
@@ -54,13 +54,6 @@ class CommentCard extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Icon(
-              Icons.favorite,
-              size: 16,
-            ),
-          )
         ],
       ),
     );
