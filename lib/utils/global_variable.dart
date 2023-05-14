599b2966104483bf6c28ce9235bf0c57ee1ec2e0
Farid Ibañez <faridibal@gmail.com>
2023-05-14 22:26:56 +0100

    arreglando fallos

--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -11,7 +11,6 @@ List<Widget> homeScreenItems = [
   const FeedScreen(),
   const SearchScreen(),
   const AddPostScreen(),
-  const Text('notifications'),
   ProfileScreen(
     uid: FirebaseAuth.instance.currentUser!.uid,
   ),
